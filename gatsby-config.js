2fc9faa9cdf32be29fe46ccc9945bda485a6a0a4
Your Name <you@example.com>
2018-06-03 20:34:11 +0900

    typographyの設定

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,11 @@
 module.exports = {
     pathPrefix: '/gatsby-test-siet'
+   ,plugins: [
+        {
+            resolve: 'gatsby-plugin-typography'
+           ,options: {
+                pathToConfigModule: 'src/utils/typography'
+	    }
+	}
+    ]
 }
---
+++ src/utils/typography.js
@@ -0,0 +1,7 @@
+// src/utils/typography.js
+import Typography from "typography"
+import bootstrapTheme from "typography-theme-bootstrap"
+
+const typography = new Typography(bootstrapTheme)
+
+export default typography
