5a01bf1ec395dfc053377bf78822888e65734597
Your Name <you@example.com>
2018-06-03 15:40:54 +0900

    ページを追加

---
+++ src/pages/gatsby/tip001.js
@@ -0,0 +1,15 @@
+// src/pages/gatsby/tip001.js
+import React from "react"
+
+export default ()=>
+    <div>
+        <h1>最初にやったことMEMO</h1>
+        <h2>gatsby new gatsby-test-site https://github.com/gatsbyjs/gatsby-starter-hello-world</h2>
+        <h2>npm install gh-pages --save-dev</h2>
+        <h2>vi package.json</h2>
+        <h2>vi gatsby-config.js</h2>
+        <h2>git init</h2>
+        <h2>git remote add origin https://github.com/ahimo/gatsby-test-site.git</h2>
+        <h2>npm run deploy</h2>
+    </div>
+
